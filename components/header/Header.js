2c3946a4598b2e90349b358c7a085657144b91b3
Alex Tomas <atomasllimos@gmail.com>
2022-10-04 17:20:04 +0200

    Add Header component

---
+++ components/header/Header.js
@@ -0,0 +1,10 @@
+import './styles.css'
+const Header = `
+    <header>
+        <h1>STAR WARS</h1>
+        <h2>Click On The Planets</h2>
+    </header>
+`
+
+
+export default Header
---
+++ main.js
@@ -0,0 +1,7 @@
+import Header from './components/header/Header'
+import './styles.css'
+
+document.querySelector('#app').innerHTML = `
+  ${Header}
+`
+
